389504301dfd1250c71e556bfd42a8cb5b9dac6e
Carmine Milieni <carmine.milieni@accenture.com>
2024-11-30 13:39:46 +0100

    first commit

---
+++ DesignCrudApiPoC.Interfaces/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DesignCrudApiPoC.Interfaces;
+
+public class Class1
+{
+}
---
+++ DesignCrudApiPoC.Interfaces/Controllers/IBaseCrudController.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignCrudApiPoC.Interfaces.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public interface IBaseCrudController<T>
+{
+    public abstract Task<ActionResult<T[]>> FindMany();
+    public abstract Task<ActionResult<T>> FindOne(int id);
+    public abstract Task<ActionResult<T>> CreateOne([FromBody] T obj);
+    public abstract Task<ActionResult<T>> UpdateOne(int id, [FromBody] T obj);
+    public abstract Task<ActionResult<T>> DeleteOne(int id);
+}
